c721ce29072ca0d3c6c36bb367f316e5ffc1f0b1
Marina <maryfairy@bk.ru>
2025-07-28 16:38:54 +0100

    add commandservice class

--- hw2/BishopPrototypeProject/src/main/java/com/bishopprototype/BishopPrototypeApplication.java
+++ hw2/BishopPrototypeProject/src/main/java/com/bishopprototype/BishopPrototypeApplication.java
@@ -1,4 +1,4 @@
-package com.weylandyutani.bishopprototype;
+package com.bishopprototype;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
