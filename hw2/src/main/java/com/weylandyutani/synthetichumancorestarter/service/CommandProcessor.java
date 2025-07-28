75015b22341e1c102bb5ccb9d97914494842d415
Marina <maryfairy@bk.ru>
2025-07-28 13:27:41 +0100

    add command processor

---
+++ hw2/src/main/java/com/weylandyutani/synthetichumancorestarter/service/CommandProcessor.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.weylandyutani.synthetichumancorestarter.service;
+
+/**
+ *
+ * @author maryf
+ */
+public class CommandProcessor {
+    
+}
